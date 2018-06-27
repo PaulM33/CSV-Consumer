a9cf847299730804a6a3ab65d95f67017a534e2d
PaulM33 <paul.magbor@gmail.com>
2018-06-27 00:02:24 -0400

    first commit

---
+++ CSV Consumer/Helper.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Linq;
+using System.Dynamic;
+using System.Text.RegularExpressions;
+
+namespace CSV_Consumer
+{
+    class Helper
+    {
+        //Load files for GUI report
+        public static string loadFile(string path)
+        {
+            var lineNum = File.ReadLines(path).Count() - 1;
+            string fileName = path + ": " + lineNum;
+
+            return fileName;
+        }
+
+        //Get information for output file
+        public static List<dynamic> getData(string path)
+        {
+            List<string> header = new List<string>();
+            List<dynamic> data = new List<dynamic>();
+            StreamReader sr = new StreamReader(path);
+            string headLine;
+            string line;
+            dynamic obj = new ExpandoObject();
+            IDictionary<string, object> dataDict = (IDictionary<string, object>)obj;
+            var lineNum = File.ReadLines(path).Count() - 1;
+
+            //Read the first line (header)
+            headLine = sr.ReadLine();
+            string[] firstLine = headLine.Split(',');
+            foreach(string l in firstLine)
+            {
+                header.Add(l);
+            }
+
+            //Read body data
+            while((line = sr.ReadLine()) != null)
+            {
+                obj = new ExpandoObject();
+                //string[] lines = line.Split(',');
+                string[] lines = Regex.Split(line, ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                for(int i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i].Length == 0)
+                        lines[i] = "[BLANK]";
+
+                    ((IDictionary<string, object>)obj).Add(new KeyValuePair<string, object>(header[i], lines[i]));
+                }
+
+                data.Add(obj);
+            }
+
+            return data;
+        }
+
+        //Output information to text file
+        public static void saveData(List<dynamic> data, string path)
+        {
+            StreamWriter sw = null;
+
+            do
+            {
+                try
+                {
+                    sw = File.AppendText(path);
+                }
+                catch(IOException) { }
+
+            } while (sw == null);
+
+            foreach(dynamic d in data)
+            {
+                IDictionary<string, object> dict = d as IDictionary<string, object>;
+
+                foreach (KeyValuePair<string, object> i in dict)
+                    sw.Write(string.Format("{0}:{1}, ", i.Key, i.Value));
+
+                sw.WriteLine();
+            }
+
+            sw.Close();
+        }
+    }
+}
---
+++ CSV Consumer/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Reflection;
+
+namespace CSV_Consumer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string folder = @".\CSV\";
+        List<string> headers = new List<string>();
+        List<dynamic> data = new List<dynamic>();
+        //string testPath = @".\CSV\test2.csv";
+        string savePath = @".\log.txt";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Load button event
+        private void loadBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Clear the listbox
+            fileList.Items.Clear();
+
+            //Get file information
+            foreach (string file in Directory.EnumerateFiles(folder, "*.csv"))
+            {
+                fileList.Items.Add(Helper.loadFile(file));
+                data = Helper.getData(file);
+                Helper.saveData(data, savePath);
+            }
+
+            MessageBox.Show("Log saved");
+        }
+        
+        //Cancel button event
+        private void cancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
